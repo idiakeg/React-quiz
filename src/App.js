d6769406fcbbd8fbd27361ef5723778da3f9da61
unknown <idiakeg@gmail.com>
2022-06-01 23:14:17 +0100

    Done with the quiz app and pusing it to github

--- src/App.js
+++ src/App.js
@@ -1,72 +1,64 @@
-import { useState } from "react";
+import React, {useState} from 'react'
 import "./App.css"
+import questions from  "./data"
+
 const App = () => {
-  const [inputValue, setInputValue] = useState({
-    first_name: "",
-    last_name: "",
-    email: ""
-  })
+  const [currentQuestion, setCurrentQuestion] = useState(0)
+  const [done, setDone] = useState(false)
+  const [score, setScore] = useState(0)
+  // const [prevScore, setPrevScore] = useState(0)
 
-  // state to manage manage the submit 
-  const [submit, setSubmit] = useState(false)
-  const [valid, setValid] = useState(false)
-
-  // handleChange function definiton
-  const handleFirstNameChange = (e) => {
-    setInputValue({...inputValue, first_name: e.target.value})
-    // setSubmit(false)
-  }
-  const handleLastNameChange = (e) => {
-    setInputValue({...inputValue, last_name: e.target.value})
-    // setSubmit(false)
-  }
-  const handleEmailChange = (e) => {
-    setInputValue({...inputValue, email: e.target.value})
-    // setSubmit(false)
-  }
-
-    let newValid = valid;
-
-
-  // handleSUbmit function definiton
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    if(inputValue.first_name && inputValue.last_name && inputValue.email){
-      setValid(true)
-      newValid = true
+  const handleBtnClick = (isCorrect) => {
+    let newScore = score + 1
+     if(isCorrect){
+      setScore(newScore)
     }
-    setSubmit(true)
-    //  console.log(submit, valid, newValid);
-       if(newValid){
-      setInputValue({
-        first_name: "",
-        last_name: "",
-        email: ""
-      })
-       newValid = false
+    // console.log(newScore, score);
+    let newQuestion = currentQuestion + 1
+    if(newQuestion < questions.length){
+      setCurrentQuestion(newQuestion)
+    }else{
+      setDone(true)
     }
+
   }
 
+  const handleRestart = () => {
+    setDone(false)
+    setCurrentQuestion(0)
+    setScore(0)
+  }
   return (
-    <div className="appContainer">
+    <div className='appContainer'>
+      {
+        done ?
+        <div className='finished'>
+          <div className="userScore">You scored <strong>{score}</strong> out of <strong>{questions.length}</strong>
+          </div> 
+          <button className='btnRestart' onClick={handleRestart}>Reset Quiz</button>
+        </div> 
         
-        <form className="formContainer" onSubmit={handleSubmit}>
-          {
-            submit && valid ? <div className="success">Your registration was sucessful!!</div> : null
-          }
-          {/* first Name */}
-          <input onChange={handleFirstNameChange} value={inputValue.first_name} className="inputField" type="text" placeholder="First Name" />
-          {submit && !newValid && !inputValue.first_name ? <span>This field cannot be left empty</span> : null }
-          {/*  last name */}
-          <input onChange={handleLastNameChange} type="text" value={inputValue.last_name}className="inputField" placeholder="Last Name" />
-          {submit &&!newValid && !inputValue.last_name ? <span>This field cannot be left empty</span> : null }
-          {/* email */}
-          <input onChange={handleEmailChange} type="text" value={inputValue.email}className="inputField" placeholder="Email" />
-          {submit && !newValid && !inputValue.email ? <span>This field cannot be left empty</span> : null }
-          <button className="btn" type="submit">Register</button>
-        </form>
+        :
+        (
+          <>
+          <div className="questionSection">
+            <div className="questionCount">
+              <span>Question {currentQuestion + 1}</span>/{questions.length}
+            </div>
+            <div className="questionText">{questions[currentQuestion].questionText}</div>
+          </div>
+          <div className="answerSection">
+            {questions[currentQuestion].answerOption.map((answer, index) => {
+              return (
+                <button onClick={() => handleBtnClick(answer.isCorrect)} key={index} className='btn'>{answer.answerText}</button>
+              )
+            }) }
+          </div>
+          </>
+        ) 
+      }
     </div>
   )
 }
 
-export default App;
+export default App
---
+++ src/data.js
@@ -0,0 +1,41 @@
+const questions = [
+    {
+        questionText: "What is the capital of france?",
+        answerOption: [
+            {answerText: "New york", isCorrect: false},
+            {answerText: "London", isCorrect: false},
+            {answerText: "Paris", isCorrect: true},
+            {answerText: "Dublin", isCorrect: false}
+        ]
+    },
+    {
+        questionText: "Who is the CEO of Tesla?",
+        answerOption: [
+            {answerText: "Jeff Bezos", isCorrect: false},
+            {answerText: "Elon Musk", isCorrect: true},
+            {answerText: "Bill Gates", isCorrect: false},
+            {answerText: "Tony Stark", isCorrect: false}
+        ]
+    },
+   
+    {
+        questionText: "The iphone was created by which company?",
+        answerOption: [
+            {answerText: "Apple", isCorrect: true},
+            {answerText: "Intel", isCorrect: false},
+            {answerText: "Amazon", isCorrect: false},
+            {answerText: "Microsoft", isCorrect: false}
+        ]
+    },
+    {
+        questionText: "How many Harry Potter books are there?",
+        answerOption: [
+            {answerText: "1", isCorrect: false},
+            {answerText: "4", isCorrect: false},
+            {answerText: "6", isCorrect: false},
+            {answerText: "7", isCorrect: true}
+        ]
+    }
+]
+
+export default questions
